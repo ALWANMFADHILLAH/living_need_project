34ccce391e81b50cc6ca28b6ebef73d21f4cc28c
nailah06 <125637533+ALWANMFADHILLAH@users.noreply.github.com>
2023-10-25 12:51:22 +0700

    sip

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
-sst - eu
+set - eu
+
 export PYTHONUNBUFFERED=true
+
 VIRTUALENV=.data/venv
+
 if [ ! -d $VIRTUALENV ]; then
-python3 -m venv $VIRTUALENV
+  python3 -m venv $VIRTUALENV
 fi
-if [ ! -f $VIRTUALENV]; then
-curl --silent --show-error --retry 5
-https://bootstrap.pyps.io/get-pip.py  $VIRTUALENV/bin/python
+
+if [ ! -f $VIRTUALENV/bin/pip ]; then
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
+
 $VIRTUALENV/bin/pip install -r requirements.txt
 $VIRTUALENV/bin/python3 app.py
